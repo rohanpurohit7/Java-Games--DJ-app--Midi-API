1b8c410e3f16bbc5843da0b5ea5bb94870023360
Rohan Purohit <rpurohit01@gmail.com>
2026-07-31 19:02:56 -0400

    Use bundled backing tracks with local playback metadata

--- src/music/backingCatalog.js
+++ src/music/backingCatalog.js
@@ -1,28 +1,82 @@
 export const BACKING_TRACKS = [
-  { id:'darlin', title:"Darlin'", artist:'JMHBM', genre:'Slow Blues Rock', key:'E', mode:'minor', bpm:58, meter:'4/4', feel:'slow-blues', source:'https://freemusicarchive.org/music/beat-mekanik/single/darlin/', license:'CC BY 4.0', chords:['Em7','Cmaj7','G','D'] },
-  { id:'strut', title:'Strut', artist:'JMHBM', genre:'Laid-back Blues Rock', key:'C', mode:'minor', bpm:73, meter:'4/4', feel:'laid-back', source:'https://freemusicarchive.org/music/beat-mekanik/single/strut/', license:'CC BY 4.0', chords:['Cm7','Fm7','Ab','G7'] },
-  { id:'dirty-dozen', title:'Dirty Dozen', artist:'JMHBM', genre:'Modern Blues Rock', key:'D', mode:'minor', bpm:83, meter:'4/4', feel:'blues-rock', source:'https://freemusicarchive.org/music/beat-mekanik/single/dirty-dozen/', license:'CC BY 4.0', chords:['Dm7','Gm7','Bb','A7'] },
-  { id:'crunch', title:'Crunch', artist:'JMHBM', genre:'Country Blues Rock', key:'E', mode:'minor', bpm:85, meter:'4/4', feel:'country-rock', source:'https://freemusicarchive.org/music/beat-mekanik/single/crunch-2/', license:'CC BY 4.0', chords:['Em','G','D','A'] },
-  { id:'super-sonic', title:'Super Sonic', artist:'JMHBM', genre:'Soul Funk Pop', key:'A', mode:'minor', bpm:97, meter:'4/4', feel:'neo-soul', source:'https://freemusicarchive.org/music/beat-mekanik/single/super-sonic/', license:'CC BY 4.0', chords:['Am9','Dm9','G13','Cmaj9'] },
-  { id:'savage', title:'Savage', artist:'JMHBM', genre:'Disco Funk', key:'Bb', mode:'minor', bpm:104, meter:'4/4', feel:'funk', source:'https://freemusicarchive.org/music/beat-mekanik/single/savage/', license:'CC BY 4.0', chords:['Bbm7','Ebm7','Gbmaj7','F7'] },
-  { id:'kinda', title:'Kinda', artist:'JMHBM', genre:'Funk Pop', key:'C', mode:'major', bpm:105, meter:'4/4', feel:'funk-pop', source:'https://freemusicarchive.org/music/beat-mekanik/single/kinda/', license:'CC BY 4.0', chords:['Cmaj7','Am7','Dm7','G13'] },
-  { id:'touch-of-funk', title:'Touch of Funk', artist:'JMHBM', genre:'Funk', key:'D', mode:'minor', bpm:130, meter:'4/4', feel:'funk', source:'https://freemusicarchive.org/music/beat-mekanik/single/touch-of-funk/', license:'CC BY 4.0', chords:['Dm9','G13','Cmaj9','A7#9'] },
-  { id:'same-old-story', title:'Same Old Story', artist:'JMHBM', genre:'Jazz R&B', key:'F#', mode:'minor', bpm:80, meter:'4/4', feel:'jazz-rnb', source:'https://freemusicarchive.org/music/beat-mekanik/single/same-old-story/', license:'CC BY 4.0', chords:['F#m9','Bm9','E13','Amaj9'] },
-  { id:'pink-soul-ii', title:'Pink Soul II', artist:'JMHBM', genre:'Soul R&B', key:'C', mode:'minor', bpm:82, meter:'3/4', feel:'soul-waltz', source:'https://freemusicarchive.org/music/beat-mekanik/single/pink-soul-ii/', license:'CC BY 4.0', chords:['Cm9','Abmaj7','Ebmaj7','G7'] },
-  { id:'enough-said', title:'Enough Said', artist:'JMHBM', genre:'Throwback Hip Hop', key:'C', mode:'minor', bpm:90, meter:'4/4', feel:'hip-hop', source:'https://freemusicarchive.org/music/beat-mekanik/single/enough-said/', license:'CC BY 4.0', chords:['Cm7','Abmaj7','Fm7','G7'] },
-  { id:'new-new', title:'New New', artist:'JMHBM', genre:'Hip Hop', key:'C', mode:'minor', bpm:91, meter:'4/4', feel:'hip-hop', source:'https://freemusicarchive.org/music/beat-mekanik/single/new-new/', license:'CC BY 4.0', chords:['Cm9','Fm9','Abmaj7','G7'] },
-  { id:'your-love', title:'Your Love', artist:'JMHBM', genre:'Soulful Trap', key:'F#', mode:'minor', bpm:93, meter:'4/4', feel:'soul-trap', source:'https://freemusicarchive.org/music/beat-mekanik/single/your-love-2/', license:'CC BY 4.0', chords:['F#m9','Dmaj7','Amaj7','Eadd9'] },
-  { id:'xxx', title:'XXX', artist:'JMHBM', genre:'R&B', key:'B', mode:'minor', bpm:140, meter:'4/4', feel:'slow-half-time', source:'https://freemusicarchive.org/music/beat-mekanik/single/xxx-1/', license:'CC BY 4.0', chords:['Bm9','Em9','Gmaj7','F#7'] },
-  { id:'intensity', title:'Intensity', artist:'JMHBM', genre:'Smooth R&B', key:'C', mode:'minor', bpm:134, meter:'4/4', feel:'slow-half-time', source:'https://freemusicarchive.org/music/beat-mekanik/single/intensity/', license:'CC BY 4.0', chords:['Cm9','Fm9','Abmaj7','G7'] },
-  { id:'pink-soul', title:'Pink Soul', artist:'JMHBM', genre:'Blues Soul', key:'C', mode:'minor', bpm:160, meter:'3/4', feel:'soul-waltz', source:'https://freemusicarchive.org/music/beat-mekanik/single/pink-soul/', license:'CC BY 4.0', chords:['Cm9','Fm9','Abmaj7','G7'] },
-  { id:'lonesome-nights', title:'Lonesome Nights', artist:'JMHBM', genre:'Southern Blues', key:'D', mode:'major', bpm:150, meter:'3/4', feel:'southern-waltz', source:'https://freemusicarchive.org/music/beat-mekanik/single/lonesome-nights/', license:'CC BY 4.0', chords:['D','G','Bm7','A7'] },
-  { id:'origin', title:'Origin', artist:'JMHBM', genre:'Boogie Woogie Blues', key:'C', mode:'major', bpm:120, meter:'4/4', feel:'boogie', source:'https://freemusicarchive.org/music/beat-mekanik/single/origin/', license:'CC BY 4.0', chords:['C7','F7','C7','G7'] },
-  { id:'life-is-good', title:'Life is Good', artist:'JMHBM', genre:'Dixieland Jazz', key:'D', mode:'major', bpm:125, meter:'4/4', feel:'swing', source:'https://freemusicarchive.org/music/beat-mekanik/single/life-is-good/', license:'CC BY 4.0', chords:['D6','B7','Em7','A7'] },
-  { id:'rusty-roads', title:'Rusty Roads', artist:'JMHBM', genre:'Country Blues Rock', key:'A', mode:'major', bpm:122, meter:'4/4', feel:'country-rock', source:'https://freemusicarchive.org/music/beat-mekanik/single/rusty-roads/', license:'CC BY 4.0', chords:['A','D','F#m','E'] }
+  {
+    id: 'funk-soul', title: 'Funk Soul', artist: 'Wiki Learning Tec de Monterrey', genre: 'Soul Funk',
+    key: 'A', mode: 'minor', bpm: 96, meter: '4/4', feel: 'neo-soul',
+    audioUrl: '/backing-tracks/funk-soul.ogg', guitar: 'electric-clean',
+    license: 'CC BY-SA 4.0', source: 'https://commons.wikimedia.org/wiki/File:Funk_Soul.ogg',
+    chords: ['Am9', 'Dm9', 'G13', 'Cmaj9']
+  },
+  {
+    id: 'e-blues', title: 'E Blues', artist: 'Michael Huber', genre: 'Slow Piano Blues',
+    key: 'E', mode: 'minor', bpm: 72, meter: '4/4', feel: 'slow-blues',
+    audioUrl: '/backing-tracks/e-blues.ogg', guitar: 'electric-jazz',
+    license: 'CC BY-SA 3.0', source: 'https://commons.wikimedia.org/wiki/File:E_Blues_by_Michael_Huber.ogg',
+    chords: ['E7', 'A7', 'E7', 'B7']
+  },
+  {
+    id: 'blues-accompaniment', title: 'Blues Accompaniment', artist: 'Jason M. C. Han', genre: 'Blues Practice Loop',
+    key: 'A', mode: 'minor', bpm: 80, meter: '4/4', feel: 'laid-back',
+    audioUrl: '/backing-tracks/blues-accompaniment.ogg', guitar: 'electric-clean',
+    license: 'CC BY-SA 4.0', source: 'https://commons.wikimedia.org/wiki/File:Blues_accompanyment.ogg',
+    chords: ['A7', 'D7', 'A7', 'E7']
+  },
+  {
+    id: 'jazz-at-the-park', title: 'Jazz at the Park', artist: 'Manwithmetalpig', genre: 'Contemporary Jazz',
+    key: 'C', mode: 'major', bpm: 92, meter: '4/4', feel: 'jazz-rnb',
+    audioUrl: '/backing-tracks/jazz-at-the-park.ogg', guitar: 'electric-jazz',
+    license: 'CC0 1.0', source: 'https://commons.wikimedia.org/wiki/File:Jazz_at_the_park.ogg',
+    chords: ['Cmaj7', 'Am7', 'Dm9', 'G13']
+  },
+  {
+    id: 'jazz-guitar', title: 'Jazz Guitar', artist: 'Serolillo', genre: 'Jazz Guitar Loop',
+    key: 'C', mode: 'major', bpm: 88, meter: '4/4', feel: 'laid-back',
+    audioUrl: '/backing-tracks/jazz-guitar.ogg', guitar: 'electric-jazz',
+    license: 'CC BY 2.5', source: 'https://commons.wikimedia.org/wiki/File:Jazz-Guitar.ogg',
+    chords: ['Cmaj7', 'A7', 'Dm7', 'G7']
+  },
+  {
+    id: 'static', title: 'Static', artist: 'Mise', genre: 'Atmospheric Soul Groove',
+    key: 'D', mode: 'minor', bpm: 84, meter: '4/4', feel: 'soul-trap',
+    audioUrl: '/backing-tracks/static.ogg', guitar: 'electric-clean',
+    license: 'CC BY 4.0', source: 'https://commons.wikimedia.org/wiki/File:Mise_-_07_-_Static.ogg',
+    chords: ['Dm9', 'Bbmaj7', 'Fmaj7', 'Cadd9']
+  },
+  {
+    id: 'jazz-piano', title: 'Jazz Piano in E-flat', artist: 'Serolillo', genre: 'Jazz Piano Loop',
+    key: 'D#', mode: 'major', bpm: 78, meter: '4/4', feel: 'jazz-rnb',
+    audioUrl: '/backing-tracks/jazz-piano.ogg', guitar: 'electric-jazz',
+    license: 'CC BY 2.5', source: 'https://commons.wikimedia.org/wiki/File:Jazz_Piano.ogg',
+    chords: ['Ebmaj7', 'Cm7', 'Fm7', 'Bb7']
+  },
+  {
+    id: 'shumi-piano', title: 'Shumi Piano', artist: 'Parchokhalq', genre: 'Soulful Piano',
+    key: 'A', mode: 'minor', bpm: 68, meter: '4/4', feel: 'slow-blues',
+    audioUrl: '/backing-tracks/shumi-piano.ogg', guitar: 'nylon-spanish',
+    license: 'CC0 1.0', source: 'https://commons.wikimedia.org/wiki/File:Shumi-Marista_Piano_Instrumental.ogg',
+    chords: ['Am', 'Fmaj7', 'C', 'G']
+  }
+];
+
+export const GUITAR_INSTRUMENTS = [
+  {
+    id: 'electric-clean', name: 'Clean Electric', family: 'Electric',
+    soundfontUrl: '/soundfonts/freepats-electric-clean.sf2', imageUrl: '/assets/studio-electric-guitar.jpg',
+    description: 'Bright sampled electric guitar with clean amplifier processing.'
+  },
+  {
+    id: 'electric-jazz', name: 'Jazz Electric', family: 'Electric',
+    soundfontUrl: '/soundfonts/freepats-electric-jazz.sf2', imageUrl: '/assets/studio-electric-guitar.jpg',
+    description: 'Warmer clean electric bank voiced for jazz and soul phrasing.'
+  },
+  {
+    id: 'nylon-spanish', name: 'Spanish Nylon', family: 'Nylon',
+    soundfontUrl: '/soundfonts/freepats-nylon-spanish.sf2', imageUrl: '/assets/studio-electric-guitar.jpg',
+    description: 'CC0 Spanish classical nylon-string guitar samples.'
+  }
 ];
 
 export function suggestedScale(track) {
-  if (track.feel.includes('funk') || track.feel === 'boogie') return track.mode === 'major' ? 'Mixolydian' : 'Dorian';
+  if (track.feel.includes('funk')) return track.mode === 'major' ? 'Mixolydian' : 'Dorian';
   if (track.feel.includes('jazz') || track.feel.includes('soul')) return track.mode === 'major' ? 'Major Pentatonic' : 'Dorian';
   return track.mode === 'major' ? 'Major Pentatonic' : 'Blues';
 }
